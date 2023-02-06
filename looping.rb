8b2cc532d8b83836f3afb019a44b4e497031ed47
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-02-06 11:16:59 +0300

    completed assignment

--- looping.rb
+++ looping.rb
@@ -1,4 +1,10 @@
 def happy_new_year
+  count = 10
+  while count >= 1
+    puts count
+    count -= 1
+  end
+  puts "Happy New Year!"
   # your code here
 end
 
@@ -16,9 +22,29 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
+  (1..100).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
   # your code here
 end
 
 def reverse_string(str)
   # your code here
+  str_index = str.length - 1
+
+  reversed_str = ""
+  while str_index >= 0
+    reversed_str += str[str_index]
+    str_index -= 1
+  end
+
+  reversed_str
 end
